64d53b84987102f5ef84d2e7ecfb0c3b9de2303a
sachinmotwani99 <63347647+sachinmotwani99@users.noreply.github.com>
2020-09-13 10:32:24 +0530

    Added the functions file to my project

---
+++ tool.js
@@ -0,0 +1,37 @@
+var table = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7,'i':8,'j':9,'k':10,'l':11,'m':12,'n':13,'o':14,'p':15,'q':16,'r':17,'s':18,'t':19,'u':20,'v':21
+              ,'w':22,'x':23,'y':24,'z':25};
+function gcd(a,b)
+{
+
+}
+function max(a,b)
+{
+	if(a>b)
+		return a;
+	else
+		return b;
+}
+function min(a,b)
+{
+	if(a>b)
+		return b;
+	else
+		return a;
+}
+function findchar(str)
+{
+	var res = str.toLowerCase();
+	var n = str.charCodeAt(0);
+	n = n-96;
+
+}
+function shiftCipher(str)
+{
+	var cip = "";
+	for(i=0;i<str.length();i++)
+	{
+		var t = findchar(str[i]);
+		cip += t;
+	}
+    doc
+}
